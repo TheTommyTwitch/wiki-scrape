9b60d60e88c4bf29caf21c53c7145cba4e6ba709
Thomas Asadurian <Tasadurian@gmail.com>
2016-03-08 16:24:04 -0800

    removed function i didnt need

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 var cheerio = require('cheerio');
 var rp = require('request-promise');
 
-getBody = function(url) {
-  var options = {
-    url: url,
-  };
-  return rp.get(options)
-    .catch(function(error) {
-      return error;
-    })
-    .then(function(response) {
-      return response;
-    });
-};
-
 var Wiki = (function() {
   function Wiki() {}
 
